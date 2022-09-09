f3e59c567ee3e6886d4bef6da597ad6c05653bb3
CodeSenpai <sipayungrian@gmail.com>
2022-09-09 20:48:55 +0700

    create seeder file

--- app/Models/Camp.php
+++ app/Models/Camp.php
@@ -10,5 +10,5 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Camp extends Model
 {
     use HasFactory, SoftDeletes;
-    protected $fillable = ['title','price'];
+    protected $fillable = ['title','slug','price'];
 }
